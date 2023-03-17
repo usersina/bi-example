e277c732c8af1dedf078f99a49b35650c165e77f
usersina <benna.anis2@gmail.com>
2023-03-17 11:24:48 +0100

    Handle duplicate data

--- compose/init/logstash/conf.d/sql/pull_data.sql
+++ compose/init/logstash/conf.d/sql/pull_data.sql
@@ -1,26 +1,18 @@
--- One row represents a product brought by a customer from an employee in a store on a day
-SELECT
-	-- Primary keys
-	products.productCode,
-    customers.customerNumber,
-    employees.employeeNumber,
-    offices.officeCode,
-    -- Values
-    orders.orderDate,
-    SUM(orderdetails.priceEach) as "SalesAmount",
-    orderdetails.quantityOrdered as "Quantity",
-    SUM(products.buyPrice) as "CostAmount"
-FROM
-	products products
-INNER JOIN 
-	orderdetails orderdetails ON products.productCode = orderdetails.productCode
-INNER JOIN
-	orders orders ON orderdetails.orderNumber = orders.orderNumber
-INNER JOIN
-	customers customers ON orders.customerNumber = customers.customerNumber
-INNER JOIN
-	employees employees ON customers.salesRepEmployeeNumber = employees.employeeNumber
-INNER JOIN
-	offices offices ON employees.officeCode = offices.officeCode
-GROUP BY
-	orders.orderNumber;
+-- Select all products brought by a customer from an employee in a store on a day
+SELECT -- Primary keys
+	products.productCode as "product_code",
+	customers.customerNumber as "customer_number",
+	employees.employeeNumber as "employee_number",
+	offices.officeCode as "office_code",
+	-- Values
+	orders.orderDate as "order_date",
+	SUM(orderdetails.priceEach) as "sales_amount",
+	orderdetails.quantityOrdered as "quantity",
+	SUM(products.buyPrice) as "cost_amount"
+FROM products products
+	INNER JOIN orderdetails orderdetails ON products.productCode = orderdetails.productCode
+	INNER JOIN orders orders ON orderdetails.orderNumber = orders.orderNumber
+	INNER JOIN customers customers ON orders.customerNumber = customers.customerNumber
+	INNER JOIN employees employees ON customers.salesRepEmployeeNumber = employees.employeeNumber
+	INNER JOIN offices offices ON employees.officeCode = offices.officeCode
+GROUP BY orders.orderNumber;
